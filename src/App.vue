7ef7ae16315b6294a3ac02c50bc2e8b1cc9fc396
imyizai <1030662977@qq.com>
2021-11-05 16:23:47 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+ <div id="app">
+   <header>
+     <Navbar />
+   </header>
+ </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+  header {
+    background: dimgrey;
+    width: auto;
+    height: 80px;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="nav">
+    <div class="logo"><img src="../assets/logo.png"></div>
+    <div class="el-menu-item" href="#">首页</div>
+    <div class="el-menu-item" href="#">资讯</div>
+    <div class="el-menu-item" href="#">视频</div>
+    <div class="el-menu-item" href="#">提问</div>
+    <div class="el-menu-item" href="#">交流</div>
+    <div class="el-menu-item" href="#">直播</div>
+    <div class="el-menu-item" href="#">供求信息</div>
+    <div class="el-menu-item" href="#">知网书屋</div>
+
+  </nav>
+</template>
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style lang="css">
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+nav .el-menu-item {
+  color:black;
+  padding: 15px 35px;
+  position: relative;
+  text-align: center;
+  border-bottom: transparent;
+  display: flex;
+  flex-direction: row;
+  transition: 0.4s;
+  font-size: larger;
+  font-weight: bold;
+}
+nav .el-menu-item:hover {
+  background-color: darkgray;
+  border-bottom: solid 6px;
+  border-bottom-color: brown;
+  height: 80px;
+}
+.nav {
+    background-color:#fff;
+}
+</style>
