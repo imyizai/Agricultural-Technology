79be51edcf40f65d8f85494ccdba2a338dde8069
imyizai <1030662977@qq.com>
2021-11-15 11:57:20 +0800

    路由配置

--- src/views/Comminicate.vue
+++ src/views/Comminicate.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="container">
-  <div v-for="(item,index) in list" :key="index"></div>
+  <div v-for="(item,index) in list" :key="index">
+    <div class="main">
+      {{item.name}}
+    </div>
+  </div>
   </div>
 </template>
 <script>
 
-// export default {
+export default {
+  data () {
+    return {
+      list: [
+        {
+          // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
+          name: '杨瑞'
+        },
+        {
+          // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
+          name: '杨瑞'
+        },
+        {
+          // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
+          name: '杨瑞'
+        },
+        {
+          // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
+          name: '杨瑞'
+        }
+      ]
+    }
+  },
+  computed: {
 
-//   data: function() {
-//       return{
-//         list: [
-//     {
-//     // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
-//      name:'杨瑞'
-//   },
-//    {
-//     // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
-//      name:'杨瑞'
-//   },
-//    {
-//     // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
-//      name:'杨瑞'
-//   },
-//    {
-//     // avatar: 'http://first-bucket20201002.oss-cn-hangzhou.aliyuncs.com/img/img/picture/me.jpg'
-//      name:'杨瑞'
-//   },
-//   ]
-//       }
-//   }
-//   // components: {},
-//   // mounted: {},
-// }
+  }
+}
+</script>
